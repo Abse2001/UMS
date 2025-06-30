68f7b4f7da50c52c74af0e45b6f25415fdb09fae
Abse <abdaslam13.2.2001@gmail.com>
2025-06-30 22:30:31 +0200

    Edit event page.

--- User Mangment System/Forms/UCevents.Designer.cs
+++ User Mangment System/Forms/UCevents.Designer.cs
@@ -36,9 +36,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(230, 48);
+            this.label1.Location = new System.Drawing.Point(197, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(292, 49);
+            this.label1.Size = new System.Drawing.Size(231, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Event Controller";
             // 
@@ -47,23 +47,26 @@
             this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.textBox1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(97, 157);
+            this.textBox1.Location = new System.Drawing.Point(83, 128);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(723, 425);
+            this.textBox1.Size = new System.Drawing.Size(620, 346);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "Developer: Shehab Ahmed Abdelaziz\r\nSoftware developer and programming student.";
+            this.textBox1.Text = "Developer: Abse\r\nCridit for help : Shehab\r\nSoftware developer and programming stu" +
+    "dent.";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // UCevents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "UCevents";
-            this.Size = new System.Drawing.Size(811, 572);
+            this.Size = new System.Drawing.Size(695, 465);
             this.ResumeLayout(false);
             this.PerformLayout();
 
